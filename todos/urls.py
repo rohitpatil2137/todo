659ff446e4204aef1f9c54ced2e90efa526284ec
rohitpatil2137 <69861855+rohitpatil2137@users.noreply.github.com>
2023-10-14 06:38:53 +0530

    Update urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
 
-app_name='todos'
+from todos.views import IndexView, app_view, delete, update, add
+
+app_name = 'todos'
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:todo_id>/delete', views.delete, name='delete'),
-    path('<int:todo_id>/update', views.update, name='update'),
-    path('add/', views.add, name='add')
+    path('', IndexView.as_view(), name='index'),
+    path('app/', app_view, name='app'),
+    path('<int:todo_id>/delete', delete, name='delete'),
+    path('<int:todo_id>/update', update, name='update'),
+    path('add/', add, name='add'),
 ]
